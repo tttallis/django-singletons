d8934fa8d2fa8ed1f60f01fd4a8825d9a6ec4263
Thomas Ashelford <thomas@ether.com.au>
2011-09-07 11:38:20 +1000

    Added change_view that creates a stub instance if admin is accessed before a singleton has been instantiated.

--- singleton_models/admin.py
+++ singleton_models/admin.py
@@ -45,3 +45,13 @@ class SingletonModelAdmin(admin.ModelAdmin):
         else:
             self.message_user(request, msg)
             return HttpResponseRedirect("../../../")
+            
+    def change_view(self, request, object_id, extra_context=None):
+        if object_id=='1':
+            self.model.objects.get_or_create(pk=1)
+        return super(SingletonModelAdmin, self).change_view(
+            request,
+            object_id,
+            extra_context=extra_context,
+        )
+
